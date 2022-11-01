1381013fb63117ce85828523b0bce0af4490ba50
Yuan Zhang <yu_an12@163.com>
2022-11-01 16:04:35 -0400

    1101.1

--- script.js
+++ script.js
@@ -4,55 +4,93 @@ let MFspan = [];
 let cursor;
 let posX = window.innerWidth / 2;
 let posY = window.innerHeight / 8;
+let x = 0;
+let y = 0;
 
-var textinput = document.getElementById("mytext");
-// textinput.setAttribute("type", "text");
-// var folder1 = gui.addFolder('Flow Field');
-
-
+p = document.getElementById("mftest");
 
 function initialize() {
-    p = document.getElementById("mftest");
-    let fontsize = 150;
+    strtest = document.getElementById("textarea").value;
 
-    strtest = 'Magnetic Field';
-    // strtest = textinput.value;
-
-    // strtest = 'MAGNETIC FIELD';
     splitstr = strtest.split('');
-
-    // let
-
-
 
     for (let i = 0; i < splitstr.length; i++) {
         // let ic = createSpan(splitstr[i])
-        let ic = document.createElement("span")
+        let ic = document.createElement("span");
+        ic.classList.add("ics");
         ic.innerHTML = splitstr[i];
         MFspan.push(ic)
-        p.append(ic);
+        p.append(MFspan[i]);
     }
-    // console.log(p);
+
     cursor = document.createElement("span");
     cursor.setAttribute("id", "cursor");
     cursor.innerHTML = "🧲";
-    // document.body.append(cursor)
-    // document.body.append(gui)
 
-
+    // console.log(p.children[0])
 }
+
 initialize()
 
-// const body = document.querySelector("body");
+// function changetext(e) {
+//     let splitstr = e.split('');
+//     // console.log(splitstr)
+//     // MFspan = [];
+//     for (let i = 0; i < splitstr.length; i++) {
+//         //     // let ic = createSpan(splitstr[i])
+//         //     // let ic = document.createElement("span")
+//         //     // ic.innerHTML = splitstr[i];
+//         // MFspan.push(splitstr[i]);
+//         //     // p.append(ic);
+//     }
+// }
 
-document.addEventListener('mousemove', (e) => {
-    console.log(textinput.value)
-    let x = e.clientX;
-    let y = e.clientY;
-    cursor.style.setProperty("--cursorx", x + "px");
-    cursor.style.setProperty("--cursory", y + "px");
-    // translate(x, y);
+// function update() {
+//     strtest = document.getElementById("textarea").value;
+//     splitstr = strtest.split('');
+//     MFspan.slice(0, MFspan.length);
+//     let texts = document.querySelectorAll('.ics')
+//     texts.forEach(box => {
+//         box.remove();
+//     });
+//     for (let i = 0; i < splitstr.length; i++) {
+//         // let ic = createSpan(splitstr[i])
+//         let ic = document.createElement("span")
+//         ic.innerHTML = splitstr[i];
+//         MFspan.push(ic)
+//         p.append(ic);
+//     }
+// }
+
+let splitstrnum;
+
+const tick = () => {
+
+    splitstrnum = splitstr.length;
+
+    strtest = document.getElementById("textarea").value;
+    splitstr = strtest.split('');
+    if (splitstrnum != splitstr.length) {
+        if (splitstrnum < splitstr.length) {
+            let ic = document.createElement("span")
+            MFspan.push(ic)
+            p.append(ic)
+        }
+        if (splitstrnum > splitstr.length) {
+            let num = splitstrnum - 1;
+            if (num > splitstr.length) {
+                p.children[2].remove;
+                num--;
+            }
+        }
+    }
+    // console.log(splitstr.length)
+    splitstrnum = splitstr.length - splitstrnum;
+    splitstrnum
     for (let i = 0; i < splitstr.length; i++) {
+        // p.childNode = splitstr.length
+        MFspan[i].innerHTML = splitstr[i]
+
 
         let posx = getOffset(MFspan[i]).left;
         let posy = getOffset(MFspan[i]).top;
@@ -61,13 +99,20 @@ document.addEventListener('mousemove', (e) => {
         let ydist = y - posy;
 
         let testleft = xdist / 160 * 45 * ydist / 160;
-        // let testw = Math.abs(1 - xdist / window.innerWidth * ydist / window.innerHeight) * 900;
         let testw = (1 - Math.abs(xdist / window.innerWidth) * 9 + 1 - Math.abs(ydist / window.innerHeight) * 9) * 900;
 
         MFspan[i].style.setProperty("--slant", testleft);
         MFspan[i].style.setProperty("--weight", testw);
 
     }
+    window.requestAnimationFrame(tick)
+}
+
+tick()
+
+document.addEventListener('mousemove', (e) => {
+    x = e.clientX;
+    y = e.clientY;
 });
 
 function getOffset(el) {
