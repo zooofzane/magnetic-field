6947056e6990b83b46b5be056408d23e18b79729
Yuan Zhang <yu_an12@163.com>
2022-11-02 18:26:05 -0400

    1102

---
+++ poster.js
@@ -0,0 +1,26 @@
+console.log('poster')
+    // let time = 1;
+let tsime = 54;
+
+
+// noise.seed(Math.random());
+
+const tickposter = () => {
+        time += 0.01
+        tsime -= 0.05
+
+        var value = perlin.get(60, time);
+
+
+
+
+
+
+        // if
+
+        // console.log(value)
+        window.requestAnimationFrame(tickposter)
+
+        // console.log(p)
+    }
+    // tickposter()
